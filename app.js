41966a42ec1a42963f9cdc5c4fe0b9aeab07c35e
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-03-26 18:29:15 +0100

    feat: second milestone

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const recipes = require('./data/recipes.js');
 const app = express();
 const port = 3000;
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send('Server del mio blog');
---
+++ data/recipes.js
@@ -0,0 +1,34 @@
+const recipes = [
+    {
+        title: 'Ciambellone',
+        content: 'Un dolce classico della tradizione italiana, perfetto per la colazione o la merenda. Soffice e profumato, si prepara con pochi ingredienti e conquista tutti con la sua semplicità.',
+        img: 'images/ciambellone.jpeg',
+        tags: [ 'dolce', 'merenda', 'colazione', 'soffice', 'facile' ]
+    },
+    {
+        title: 'Cracker alla barbabietola',
+        content: 'Snack croccanti e salutari, dal colore vibrante grazie alla barbabietola. Perfetti per accompagnare hummus, formaggi o semplicemente da sgranocchiare.',
+        img: 'images/cracker_barbabietola.jpeg',
+        tags: [ 'snack', 'salutare', 'barbabietola', 'croccante', 'vegan' ]
+    },
+    {
+        title: 'Pane fritto dolce',
+        content: 'Una versione golosa del classico pane fritto, arricchito con zucchero e cannella. Croccante fuori e morbido dentro.',
+        img: 'images/pane_fritto_dolce.jpeg',
+        tags: [ 'dolce', 'fritto', 'merenda', 'cannella', 'goloso' ]
+    },
+    {
+        title: 'Pasta alla barbabietola',
+        content: 'Un primo piatto colorato e cremoso, con una salsa a base di barbabietola e ricotta. Un mix di dolcezza e sapidità che sorprende al primo assaggio.',
+        img: 'images/pasta_barbabietola.jpeg',
+        tags: [ 'primo', 'barbabietola', 'cremoso', 'veloce', 'vegetariano' ]
+    },
+    {
+        title: 'Torta paesana',
+        content: 'Un dolce rustico a base di pane raffermo, cacao e latte, arricchito con noci, mandorle e uvetta per un tocco croccante e goloso.',
+        img: 'images/torta_paesana.jpeg',
+        tags: [ 'dolce', 'tradizionale', 'cioccolato', 'futta secca', 'rustico' ]
+    }
+];
+
+module.exports = recipes;
